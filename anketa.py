89533272b579f2696a4e5d47630a081a04aac7a8
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-08-02 09:55:14 +0300

    Update anketa.py

--- anketa.py
+++ anketa.py
@@ -102,10 +102,22 @@ st.session_state['Письмо'] = st.checkbox('Письмо')
 st.session_state['Звонок'] = st.checkbox('Звонок')
 st.session_state['Встреча'] = st.checkbox('Встреча')
 st.session_state['Приоритет обработки'] = st.radio('Приоритет обработки', options=('A', 'B', "C"), horizontal=True)
+import json
+def write_json(new_data, filename='ankets.json'):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["consumer_info"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+ 
+    # python object to be appended
+
 def json_click():
-    import json
-    with open('ankets.json', 'a', encoding='utf-8') as fp:
-        json.dump({f'{"_".join(st.session_state["ФИО"].split())}_{"_".join(st.session_state["Компания"].split())}': dict(st.session_state)}, fp)
+    write_json({f'{"_".join(st.session_state["ФИО"].split())}_{"_".join(st.session_state["Компания"].split())}': dict(st.session_state)})
     import time
     time.sleep(1)
     from streamlit_js_eval import streamlit_js_eval
