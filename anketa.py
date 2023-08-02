bd0c7625ec9fcb29de1dfafcb8b79cc7a2beb532
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-08-02 10:11:14 +0300

    Update anketa.py

--- anketa.py
+++ anketa.py
@@ -108,20 +108,20 @@ def write_json(new_data, filename='jsons_ankets/ankets.json'):
           # First we load existing data into a dict.
         file_data = json.load(file)
         # Join new_data with file_data inside emp_details
-        st.write(file_data)
-        # file_data["consumer_info"].append(new_data)
+        # st.write(file_data)
+        file_data["consumer_info"].append(new_data)
         # # Sets file's current position at offset.
-        # file.seek(0)
+        file.seek(0)
         # # convert back to json.
-        # json.dump(file_data, file, indent = 4)
+        json.dump(file_data, file, indent = 4)
  
     # python object to be appended
 
 def json_click():
     write_json({f'{"_".join(st.session_state["ФИО"].split())}_{"_".join(st.session_state["Компания"].split())}': dict(st.session_state)})
     import time
-    time.sleep(1000)
+    time.sleep(1)
     from streamlit_js_eval import streamlit_js_eval
-    ############################################################################################################################### streamlit_js_eval(js_expressions="parent.window.location.reload()")
+    streamlit_js_eval(js_expressions="parent.window.location.reload()")
 st.button("Отправить", on_click=json_click)
 
