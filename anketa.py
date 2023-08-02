92b832626b00a6ac8c24502e1992924c12396b2a
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-08-02 10:05:23 +0300

    Update anketa.py

--- anketa.py
+++ anketa.py
@@ -108,7 +108,7 @@ def write_json(new_data, filename='jsons_ankets/ankets.json'):
           # First we load existing data into a dict.
         file_data = json.load(file)
         # Join new_data with file_data inside emp_details
-        st.write(file_data.keys())
+        st.write(file_data)
         # file_data["consumer_info"].append(new_data)
         # # Sets file's current position at offset.
         # file.seek(0)
